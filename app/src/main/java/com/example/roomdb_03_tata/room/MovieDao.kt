b1ac3bbba80b70d69dc466bcf99fa1f66115973c
AnandaRegitaC <anandaregitac@gmail.com>
2022-01-26 15:52:22 +0700

    Initial commit

---
+++ app/src/main/java/com/example/roomdb_03_tata/room/MovieDao.kt
@@ -0,0 +1,24 @@
+package com.example.roomdb_03_tata.room
+
+import androidx.room.*
+
+@Dao
+interface MovieDao {
+
+    @Insert
+    suspend fun addMovie(movie: Movie)
+
+    @Update
+    suspend fun updateMovie(movie: Movie)
+
+    @Delete
+    suspend fun deleteMovie(movie: Movie)
+
+    @Query (
+        "SELECT * FROM movie"
+    )
+    suspend fun getMovie():List<Movie>
+
+    @Query ("SELECT * FROM movie WHERE id=:movie_id")
+    suspend fun getMovie(movie_id : Int) : List<Movie>
+}
